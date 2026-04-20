d8132884cb0af66a03ec9fb2fe3985acc208a21f
gambtho <Thomas.Gamble@microsoft.com>
2026-04-20 00:05:45 -0400

    feat(cli): add cronfoundry binary skeleton with admin subcommand tree

---
+++ cmd/cronfoundry/main.go
@@ -0,0 +1,25 @@
+// Package main is the CronFoundry service CLI. Starting in P2a it hosts the
+// `admin` subcommand; P2c will absorb the P1 runner binary here as well.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	root := &cobra.Command{
+		Use:           "cronfoundry",
+		Short:         "CronFoundry service CLI",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+	}
+	root.AddCommand(newAdminCmd())
+
+	if err := root.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
